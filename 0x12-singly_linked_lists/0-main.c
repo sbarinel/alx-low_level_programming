23e63854f9829110dda54c80af71c6b46201d155
sbarinel <sabelasatey@gmail.com>
2023-08-29 08:29:22 -0700

    update3

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -13,6 +13,7 @@ int main(void)
 	list_t *head;
 	list_t *new;
 	list_t hello = {"World", 5, NULL};
+	size_t n;
 
 	head = &hello;
 	new = malloc(sizeof(list_t));
@@ -36,5 +37,4 @@ int main(void)
 
 	free(new);
 	return (0);
-
 }
--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+  * print_list - Prints ALL elements of linked lisk
+  * @h: POINTER to the list_t to print
+  *
+  * Return : NUMBER of printed NODES
+  */
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
 	while (h)
 	{
-		if (h->str)
+		if (!h->str)
 			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
