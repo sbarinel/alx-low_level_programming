7c8a20ad169aab5e0fc8e4fd6ef9a68ed3511de2
sbarinel <sabelasatey@gmail.com>
2023-08-29 08:58:57 -0700

    update4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[%u] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
-
-		h = h->next;
-		s++;
+	if (!h->str)
+	printf("[%u] %s\n", h->len, h->str);
+	else
+	printf("[0] (nil)\n");
+	h = h->next;
+	s++;
 	}
 
 	return (s);
