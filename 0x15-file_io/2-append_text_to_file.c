5faa66be53c44fc6c1ea418d890e827164237060
sbarinel <sabelasatey@gmail.com>
2023-09-04 16:33:55 -0700

    task1 update

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+  * append_text_to_file - Appends txt at end of file
+  * @filename: filename
+  * @text_content: txt cntnt
+  *
+  * Return: 1 on success and -1 on failure
